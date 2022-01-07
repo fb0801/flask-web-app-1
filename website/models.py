3a58a5d9d39da375a23958ec7e38eb9150f26613
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-01-07 21:20:55 +0000

    Add files via upload

--- website/models.py
+++ website/models.py
@@ -1,1 +1,9 @@
+from enum import unique
 from . import db 
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    #props for the user tbl
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
