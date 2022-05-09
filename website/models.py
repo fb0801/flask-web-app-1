5b59c361b035d0d4636099976ec26f2dccb8161e
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-05-09 21:41:54 +0100

    Add files via upload

--- website/models.py
+++ website/models.py
@@ -6,8 +6,8 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class Note(db.Model):
-    id=db.Coloumn(db.Integer, primary_key=True)
-    data = db.Coloumn(db.String(10000))
+    id=db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
