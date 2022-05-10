446e7d8e551ab607ebbec673214c7e33b83c7689
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-05-10 21:37:41 +0100

    Add files via upload

--- website/auth.py
+++ website/auth.py
@@ -2,13 +2,28 @@ import re
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    return render_template('login.html')
+    if request.method =='POST':
+        email = request.form.get("email")
+        password = request.get("password")
+
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category="success")
+            else:
+                flash("incorrect, try again!", category="error")
+        else:
+            flash("email does not exist!", category="error")
+    return render_template("login.html", booLean= True)
+   # data = request.form
+  #  return render_template('login.html')
 
 
 @auth.route('/logout')
@@ -19,20 +34,23 @@ def logout():
 def sign_up():
     if request.method=="POST":
         email= request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1= request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("email already exists", category="error")
+        elif len(email) < 4:
             flash('Email must be greater than 3 character', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('firstname must be greater than 1 character', category='error')
         elif password1 != password2:
             flash('passwords dont match', category='error')
         elif len(password1) < 7:
             flash('password must be at least 7 characters', category='error')
         else:
-            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1,method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1,method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account made!', category='success')
