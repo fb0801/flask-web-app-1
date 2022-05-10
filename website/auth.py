23a8ddb21188cb5baa52cc1f30dabc6ba6fbd225
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-05-10 21:16:49 +0100

    Add files via upload
    creating user

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+import re
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -29,7 +32,10 @@ def sign_up():
         elif len(password1) < 7:
             flash('password must be at least 7 characters', category='error')
         else:
-            
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1,method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account made!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
